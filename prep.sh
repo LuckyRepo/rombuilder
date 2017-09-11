6c9aa0d2d446c7736284c00b64a715682db814b9
LuckyRepo <derrick@klabouch.com>
2017-09-11 13:41:23 -0400

    Change sync options to fix loop
    Old method loops and breaks script. New calls for break of case operation in No option.

--- prep.sh
+++ prep.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 	clear
-	echo -e '\e[104mRomBuilder by LuckyRepo\e[0m'
+	echo -e '\e[104mRomBuilder by LuckyRepo v0.0.1\e[0m'
 	echo -e '\e[91mThis build still contains test code. Not for daily use. Devs and testers Only.\e[0m'
 # Set PATH for all REPO commands
 	PATH=~/bin:$PATH
@@ -19,13 +19,13 @@
             echo -e '\e[32mSet Jack Server to -Xmx6g\e[0m'
     cd $OLDPWD
 # Ask if they want to sync in yes or no
-	read -p "Do you want to sync before building? y/n" syncchoice
-	case "$syncchoice" in 
-  		y|Y ) repo sync --forcesync;;
-  		n|N ) echo -e '\e[96mSkipping Sync\e[0m';;
-		b|B ) repo sync -c -f -j8 --force-sync --no-clone-bundle --no-tags
-  	* ) echo "invalid";;
-	esac
+	echo "Do you wish to install this program?"
+	select yn in "Yes" "No"; do
+    		case $yn in
+        		Yes ) echo -e '\e[96mStarting Sync\e[0m'; repo sync --force-sync;;
+        		No ) echo -e '\e[96mSkipping Sync\e[0m'; break;;
+    		esac
+	done
 # Start Build leaving lunch and make for user
 	    echo -e '\e[32mMarking start of build\e[0m'
             sleep 1
